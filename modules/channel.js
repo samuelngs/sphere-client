26e09e94177824615ebec5a48225a5f26f2f2841
Samuel Ng <sam@infinitely.io>
2016-01-07 22:26:39 -0700

    new channel class

---
+++ modules/channel.js
@@ -0,0 +1,11 @@
+;(function() {
+
+    var global = this;
+
+    var Channel = function Channel(options) {
+        Sphere.Core.Base.call(this, options);
+    };
+
+    Sphere.Module.Channel = Channel;
+
+}.call(this || window));
