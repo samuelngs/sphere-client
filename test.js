2c43bcb986b125636150bec3a022be239277ad42
Samuel Ng <sam@infinitely.io>
2016-01-09 22:40:56 -0700

    add basic event handlers for websocket client

--- test.js
+++ test.js
@@ -6,4 +6,16 @@
 
     websocket = new Sphere.Module.WebSocket("ws://localhost:4000/sync");
 
+    websocket.on('open', function() {
+        console.log('connection opened');
+    });
+
+    websocket.on('close', function() {
+        console.log('connection closed');
+    });
+
+    websocket.on('message', function(packet) {
+        console.log('message:', packet);
+    });
+
 }.call(window || this))
