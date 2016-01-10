8df2619146510d80ad06d86861cb9beb186b0ec6
Samuel Ng <sam@infinitely.io>
2016-01-10 15:51:10 -0700

    new js build script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if ! which node > /dev/null; then
+    echo "nodejs is not installed"
+    exit 1
+fi
+
+if ! which npm > /dev/null; then
+    echo "npm is not installed"
+    exit 1
+fi
+
+if ! which uglifyjs > /dev/null; then
+    echo "uglify is not install, please install it with command 'npm install -g uglify'"
+fi
+
+uglifyjs ./sphere.js \
+         ./core/base.js \
+         ./modules/websocket.js \
+         ./modules/packet.js \
+         ./modules/message.js \
+         ./modules/channel.js \
+         -o sphere.min.js \
+         --source-map sphere.min.js.map \
+         -p 5 -c -m
